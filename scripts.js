ee89e8ea16929573303cf4e9cb7b868f6ea71830
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-05-14 17:52:04 +0300

    add FQA page and edit in home page

--- scripts.js
+++ scripts.js
@@ -13,3 +13,50 @@ function show(){
     document.getElementsByTagName('article')[0].classList.toggle("blurWindow");
     console.log('aaaa');
 }
+
+
+
+
+/* FAQ Section */ 
+
+// function display(id,section){
+//   document.querySelector(".display-hide-content").classList.add("display");
+//   document.querySelector(".close-button").classList.add("display");
+//   document.getElementById(id).style.display = "none";
+//   document.querySelector(".question-number").classList.add("number-color");
+//   document.querySelector(section).classList.add("before");
+// }
+
+// function nonDisplay(id,section){
+//   document.querySelector(".display-hide-content").classList.remove("display");
+//   document.querySelector(".close-button").classList.remove("display");
+//   document.getElementById(id).style.display = "flex";
+//   document.querySelector(".question-number").classList.remove("number-color");
+//   document.querySelector(section).classList.remove("before");
+// }
+
+
+function newDisplay(parentId){
+  const parent=document.getElementById(parentId);
+  parent.querySelector(".display-hide-content").classList.add("display");
+  parent.querySelector(".close-button").classList.add("display");
+  parent.querySelector(".question-number").classList.add("number-color");
+  parent.querySelector('.open-button').style.display="none";
+  parent.classList.add("before");
+}
+
+function newNonDisplay(parentId){
+  const parent=document.getElementById(parentId);
+  parent.querySelector(".display-hide-content").classList.remove("display");
+  parent.querySelector(".close-button").classList.remove("display");
+  parent.querySelector(".question-number").classList.remove("number-color");
+  parent.querySelector('.open-button').style.display="flex";
+  parent.classList.remove("before");
+}
+
+// document.querySelector(".close-button").addEventListener("click", function (){
+//   document.querySelector(".display-hide-content").classList.toggle("non-display");
+// });
+
+
+
