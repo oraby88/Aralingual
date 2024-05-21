e8ef560de232abbe177878dfc6038dd182ddcfe6
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-05-21 17:59:37 +0300

    finish contactUs and error404 pages and editing in all pages

--- scripts.js
+++ scripts.js
@@ -1,23 +1,20 @@
 function appear() {
-    document.querySelector(".dropDown").classList.toggle("display");
-  }
-  function mini_nav_dropDown() {
-    document.querySelector(".mini-nav-dropDown").classList.toggle("display");
-  }
+  document.querySelector(".dropDown").classList.toggle("display");
+}
+function mini_nav_dropDown() {
+  document.querySelector(".mini-nav-dropDown").classList.toggle("display");
+}
 
 var humbrger = document.querySelector(".humbrger");
 var navList = document.querySelector(".nav-list");
 
-function show(){
-    document.getElementById('nav-list').classList.toggle("nav-display");
-    document.getElementsByTagName('article')[0].classList.toggle("blurWindow");
-    console.log('aaaa');
+function show() {
+  document.getElementById("nav-list").classList.toggle("nav-display");
+  document.getElementsByTagName("article")[0].classList.toggle("blurWindow");
+  console.log("aaaa");
 }
 
-
-
-
-/* FAQ Section */ 
+/* FAQ Section */
 
 // function display(id,section){
 //   document.querySelector(".display-hide-content").classList.add("display");
@@ -35,22 +32,21 @@ function show(){
 //   document.querySelector(section).classList.remove("before");
 // }
 
-
-function newDisplay(parentId){
-  const parent=document.getElementById(parentId);
+function newDisplay(parentId) {
+  const parent = document.getElementById(parentId);
   parent.querySelector(".display-hide-content").classList.add("display");
   parent.querySelector(".close-button").classList.add("display");
   parent.querySelector(".question-number").classList.add("number-color");
-  parent.querySelector('.open-button').style.display="none";
+  parent.querySelector(".open-button").style.display = "none";
   parent.classList.add("before");
 }
 
-function newNonDisplay(parentId){
-  const parent=document.getElementById(parentId);
+function newNonDisplay(parentId) {
+  const parent = document.getElementById(parentId);
   parent.querySelector(".display-hide-content").classList.remove("display");
   parent.querySelector(".close-button").classList.remove("display");
   parent.querySelector(".question-number").classList.remove("number-color");
-  parent.querySelector('.open-button').style.display="flex";
+  parent.querySelector(".open-button").style.display = "flex";
   parent.classList.remove("before");
 }
 
@@ -58,5 +54,6 @@ function newNonDisplay(parentId){
 //   document.querySelector(".display-hide-content").classList.toggle("non-display");
 // });
 
-
-
+function NumberLanguageAppear() {
+  document.querySelector(".phoneDropdown").classList.toggle("display");
+}
